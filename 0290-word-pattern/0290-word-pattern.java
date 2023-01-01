6bcd19576dc86e6f4889984cef89a158fb1f26e6
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-01-01 09:07:21 +0530

    Time: 1 ms (95.36%), Space: 40.5 MB (66.98%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String p, String s) {
+         HashMap<Character, String> hm=new HashMap<>();
+        HashSet<String> set=new HashSet<>();
+         String word[]=s.split(" ");
+        if(p.length()!=word.length)
+            return false;
+        for(int i=0;i<p.length();i++){
+            char c=p.charAt(i);
+            if(hm.containsKey(c)){
+                if(!hm.get(c).equals(word[i]))
+                    return false;
+            }
+            else{
+                if(set.contains(word[i]))
+                    return false;
+                hm.put(c,word[i]);
+                set.add(word[i]);
+            }
+        } 
+        return true;
+        
+    }
+}
