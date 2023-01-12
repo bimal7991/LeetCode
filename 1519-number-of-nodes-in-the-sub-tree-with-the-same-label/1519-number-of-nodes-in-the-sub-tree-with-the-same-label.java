16d49b6d279414886ccf2e6f42408bbb05681c73
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-01-12 10:54:25 +0530

    Time: 56 ms (100.00%), Space: 113.9 MB (95.65%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -7,18 +7,18 @@ class Solution {
             list[arr[0]].add(arr[1]);
             list[arr[1]].add(arr[0]);
         }
+        int vis[]=new int[n];
         int  res[]=new int[n];
-        dfs(0,-1,list,labels,res);
-        return res;
-        
-        
+        dfs(0,vis,list,labels,res);
+        return res;    
     }
-    public int[] dfs( int node,int parent,ArrayList<Integer> []list,String labels,int res[]){
-        int arr[]=new int[26];    
+    public int[] dfs( int node,int vis[],ArrayList<Integer> []list,String labels,int res[]){
+        int arr[]=new int[26]; 
+        vis[node]=1;
         for(int child:list[node]){
-            if(child==parent)
+            if(vis[child]==1)
                 continue;
-            int temp[]=dfs(child,node,list,labels,res);
+            int temp[]=dfs(child,vis,list,labels,res);
             for(int i=0;i<26;i++){
                 arr[i]=arr[i]+temp[i];
             }
