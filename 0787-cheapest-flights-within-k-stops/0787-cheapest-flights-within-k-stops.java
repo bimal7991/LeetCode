8a081cdff021ef673ec51c7f309843ddbbbb2443
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-01-27 00:04:16 +0530

    Time: 13 ms (34.50%), Space: 47.9 MB (12.80%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,53 +1,61 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-           List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] flight : flights) {
-            adj.get(flight[0]).add(new int[] {flight[1], flight[2]});
-        }
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {src, 0});
-        int[] minCost = new int[n];
-        Arrays.fill(minCost, Integer.MAX_VALUE);
-        int stops = 0;
-        while (!q.isEmpty() && stops <= k) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] curr = q.poll();
-                for (int[] neighbour : adj.get(curr[0])) {
-                    int price = neighbour[1], neighbourNode = neighbour[0];
-                    if (price + curr[1] >= minCost[neighbourNode]) continue;
-                    minCost[neighbourNode] = price + curr[1];
-                    q.offer(new int[] {neighbourNode, minCost[neighbourNode]});
-                }
-            }
-            stops++;
-        }
-        return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
-    }
-    void sortestDistance(int node, ArrayList<Integer>[] arr, int[] dis, HashMap<String, Integer> weight,int k) {	
-			Queue<Integer> queue=new LinkedList<Integer>();
-			dis[node]=0;
-			queue.add(node);
+           ArrayList<ArrayList<Pair>> arr=new ArrayList<>();
+  			for(int i=0;i<n;i++) {
+  				arr.add(new ArrayList<Pair>());
+  			}
+  			
+  			for(int fl[]:flights) {
+  				arr.get(fl[0]).add(new Pair(fl[1],fl[2]));
+  			}
+            int dis[]=new int[n];
+  			Arrays.fill(dis, Integer.MAX_VALUE);
+  			Queue<Tuple> queue=new LinkedList<Tuple>();
+			dis[src]=0;
+			queue.add(new Tuple(0,src,0));
 			//System.out.println(node);
-        int stops=0;
-			while(!queue.isEmpty() && stops<=k) {
-				
-                int size=queue.size();
-                while(size-->0){
-                int curr=queue.peek();
-				queue.remove();
-				for(int child:arr[curr]) {
+            int stops=0;
+			while(!queue.isEmpty()) {
+				Tuple t=queue.peek();
+                queue.remove();
+                int stop=t.a;
+                int cur=t.b;
+                int d=t.c;
+                if(stop>k)
+                    continue;
+                
+				for(Pair child:arr.get(cur)) {
+                    int adjNode = child.a; 
+                    int edW = child.b; 
 					//System.out.println(weight.get(new Weight(curr,child)));
-					if(dis[curr]+weight.get(""+curr+","+child)<dis[child]) {
+					if(d+edW<dis[adjNode] && stop<=k) {
 						//System.out.println(child);
-						queue.add(child);
-						dis[child]=dis[curr]+weight.get(""+curr+","+child);
-					}
-				  }
-                }
-                stops++;
+						queue.add(new Tuple(stop+1,adjNode,d+edW));
+						dis[adjNode]=d+edW;
+				}
 				
 			}
 		}
+          
+        
+          return dis[dst]==Integer.MAX_VALUE?-1:dis[dst];
+        
+    }
+}
+class Pair{
+    int a,b;
+    Pair(int a,int b){
+       this.a=a;
+        this.b=b; 
+    }
+    Pair(){}
+}
+class Tuple{
+    int a,b,c;
+    Tuple(int a,int b,int c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+    Tuple(){}
 }
