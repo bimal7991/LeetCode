0e5a98adaf238083729f573b628f4407a9a1f303
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-02-14 09:00:45 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (53.88%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -2,21 +2,21 @@ class Solution {
     public String addBinary(String a, String b) {
         char arr1[]=a.toCharArray();
         char arr2[]=b.toCharArray();
-        ArrayList<Character> ch=new ArrayList<>();
-        int c=0;
+        StringBuilder sb = new StringBuilder();
         int n=a.length()-1,m=b.length()-1;
+        int c=0;
         while(n>=0 && m>=0){
             int sum=arr1[n]-'0'+arr2[m]-'0'+c;
             if(sum<2){
-                ch.add((char)(sum+'0'));
+                sb.append(sum);
                 c=0;
             }
             else if(sum==2){
-                ch.add('0');
+                sb.append(0);
                 c=1;
             }
             else if(sum==3){
-                ch.add('1');
+                sb.append(1);
                 c=1;
             }
             n--;
@@ -25,15 +25,15 @@ class Solution {
         while(n>=0){
             int sum=arr1[n]-'0'+c;
             if(sum<2){
-                ch.add((char)(sum+'0'));
+                sb.append(sum);
                 c=0;
             }
             else if(sum==2){
-                ch.add('0');
+                sb.append(0);
                 c=1;
             }
             else if(sum==3){
-                ch.add('1');
+                sb.append(1);
                 c=1;
             }
             n--;
@@ -41,30 +41,22 @@ class Solution {
         while(m>=0){
             int sum=arr2[m]-'0'+c;
             if(sum<2){
-                ch.add((char)(sum+'0'));
+                sb.append(sum);
                 c=0;
             }
             else if(sum==2){
-                ch.add('0');
+                sb.append(0);
                 c=1;
             }
             else if(sum==3){
-                ch.add('1');
+                sb.append(1);
                 c=1;
             }
             m--;
         }
         if(c==1)
-            ch.add('1');
-        Collections.reverse(ch);
-        StringBuilder sb = new StringBuilder();
- 
-        // Appends characters one by one
-        for (Character c1 : ch) {
-            sb.append(c1);
-        }
-        
-        return sb.toString();
+          sb.append(1);
+        return sb.reverse().toString();
         
     }
 }
