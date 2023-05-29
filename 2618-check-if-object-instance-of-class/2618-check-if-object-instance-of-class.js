8e28fe89a6c9c835d7257b80c9a0bed5cb1af599
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-05-29 08:11:53 +0530

    Time: 101 ms (82.41%), Space: 53.1 MB (21.27%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -1,12 +1,21 @@
 /**
- * @param {any} object
+ * @param {any} obj
  * @param {any} classFunction
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    if(obj === null || obj === undefined || typeof classFunction !== 'function')
-        return false;
-    return Object(obj) instanceof classFunction;
+    while(obj!=null)
+    {
+        if(obj.constructor === classFunction)
+        {
+            return true;
+        }
+
+        obj = Object.getPrototypeOf(obj);
+
+    }
+
+    return false;
 };
 
 /**
