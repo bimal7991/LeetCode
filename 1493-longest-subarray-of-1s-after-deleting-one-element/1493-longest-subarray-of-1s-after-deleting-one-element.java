367a517e117ec77c7d2e056a2b199a778cf86b22
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-07-05 09:45:46 +0530

    Time: 1 ms (99.93%), Space: 53.3 MB (19.88%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,24 +1,21 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)
-               list.add(i);                
+        int prev=0,curr=0,j=0;
+        int res=0;
+        while(j<nums.length){
+            if(nums[j]==1)
+                curr++;
+            else{
+                res=Math.max(res,curr+prev);
+                prev=curr;
+                curr=0;
+            }
+            j++;
         }
-        int prev=-1;
-        int max=0;
-        for(int i=0;i<list.size();i++){
-            int cur=list.get(i);
-            int next=nums.length;
-            if(i+1<list.size())
-            next=list.get(i+1);
-            
-            int val=cur-(prev+1)+(next-(cur+1));   
-            max=Math.max(val,max);
-            prev=cur;
-        }
-        if(list.size()<=1)
-           return nums.length-1;
-        return max;
+         res=Math.max(res,curr+prev);
+        if(res==nums.length)
+            res--;
+        return res;
+        
     }
 }
