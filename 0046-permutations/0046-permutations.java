7e4109092a532f302f6cc64781b4ea70a1b81f60
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-08-02 09:35:56 +0530

    Time: 1 ms (98.58%), Space: 44 MB (63.60%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,29 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list=new ArrayList<>();
-        List<Integer> sub=new ArrayList<>();
-        permutate(0,list,nums);
+        
+        helper(nums,list,0);
         return list;
     }
-    void permutate(int index,  List<List<Integer>> list,int nums[]){
-        if(index==nums.length){
+    void helper(int nums[],List<List<Integer>> list,int k){
+        if(k==nums.length){
             List<Integer> sub=new ArrayList<>();
-            for(int i=0;i<nums.length;i++){
-                sub.add(nums[i]);
-            }
+            for(int i:nums)
+                sub.add(i);
             list.add(new ArrayList<>(sub));
             return;
         }
-        
-        for(int i=index;i<nums.length;i++){
-            swap(i,index,nums);
-            permutate(index+1,list,nums);
-            swap(i,index,nums);
+        for(int i=k;i<nums.length;i++){
+            swap(nums,i,k);
+            helper(nums,list,k+1);
+            swap(nums,i,k);
         }
-        
-        
     }
-    void swap(int i,int j,int nums[]){
+    void swap(int nums[],int i,int j){
         int t=nums[i];
         nums[i]=nums[j];
         nums[j]=t;
