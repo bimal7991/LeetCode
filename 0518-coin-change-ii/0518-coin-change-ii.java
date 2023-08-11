b5e483181b9beef195803989650eeca58652a81b
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-08-11 10:59:46 +0530

    Time: 7 ms (58.33%), Space: 45.7 MB (44.77%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int dp[][]=new int[coins.length+1][amount+1];
+       dp[0][0]=1;
+        
+        for(int i=1;i<=coins.length;i++){
+            dp[i][0]=1;
+            for(int j=0;j<=amount;j++){
+               
+                if(j>=coins[i-1]){
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+                    
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+                
+            }
+        }
+        
+       
+        return dp[coins.length][amount];
+       
+    }
+}
