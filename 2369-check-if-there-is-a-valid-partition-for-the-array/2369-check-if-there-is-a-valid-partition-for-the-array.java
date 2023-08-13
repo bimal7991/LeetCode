db6b7b2d46a8724c55c5f2349607de5be5e671b7
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-08-13 12:57:28 +0530

    Time: 6 ms (92.23%), Space: 61.4 MB (35.92%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean validPartition(int[] nums) {
-        return helper(nums,nums.length-1);
+        
+        boolean dp[]=new boolean[nums.length];
+        return helper(nums,nums.length-1,dp);
     }
-    boolean helper(int[] nums,int i){
+    boolean helper(int[] nums,int i,boolean dp[]){
         if(i<0)
             return true;
+        if(dp[i])
+            return true;
       boolean ans=false;
+        
        if(i-1>=0 && nums[i]==nums[i-1])
-          ans=ans || helper(nums,i-2);
+          ans=ans || helper(nums,i-2,dp);
         
        if(i-2>=0 && (nums[i]==nums[i-1]+1 && nums[i]==nums[i-2]+2 && nums[i-1]==nums[i-2]+1))
-            ans=ans || helper(nums,i-3);
+            ans=ans || helper(nums,i-3,dp);
        if(i-2>=0 && (nums[i]==nums[i-1] && nums[i]==nums[i-2]))
-            ans=ans || helper(nums,i-3);
+            ans=ans || helper(nums,i-3,dp);
         
-       if(ans)
-          return true;
-        else
-        return  false;
+      return dp[i]=ans;
     }
 }
