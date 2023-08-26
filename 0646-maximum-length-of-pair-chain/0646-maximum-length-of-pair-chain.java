c7eb42f3f246a2311c069eeff0008e3a84ea7184
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2023-08-26 11:09:56 +0530

    Time: 8 ms (99.48%), Space: 43.7 MB (72.14%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -7,20 +7,11 @@ class Solution {
         for(int i=0;i<pairs.length;i++){
             if(pairs[i][0]>end){
                 end=pairs[i][1];
-                start=pairs[i][0];
                 count++;
-                if(i>0)
-                dp[i]=Math.max(dp[i-1]+1,count);
-                else
-                    dp[i]=1;
-            }
-            else{
-                if(i>=1)
-                dp[i]=dp[i-1];
                 
             }
             
         }
-        return dp[pairs.length-1];
+        return count;
     }
 }
