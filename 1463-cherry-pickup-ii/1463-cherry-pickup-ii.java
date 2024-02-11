6dc0f48b95305fee25c380457a56ee3a2a6de576
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-02-11 11:08:23 +0530

    Time: 14 ms (99.12%), Space: 45.6 MB (69.54%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -7,7 +7,31 @@ class Solution {
             for(int a[]:d)
                 Arrays.fill(a,-1);
         }
-        return helper(grid,0,0,grid[0].length-1,dp);
+        // return helper(grid,0,0,grid[0].length-1,dp);
+        dp[0][0][n-1]=grid[0][0]+grid[0][n-1];
+       int ans=-1;
+        for(int row=1;row<m;row++){
+             
+            for(int a=0;a<n;a++){
+                for(int b=a+1;b<n;b++){
+                    int max=-1;
+                     for(int i=Math.max(a-1,0);i<=Math.min(n-1,a+1);i++){
+                         for(int j=Math.max(b-1,0);j<=Math.min(n-1,b+1);j++){
+                          if(i<j)
+                           max=Math.max(max,dp[row-1][i][j]);
+                         }
+                        }
+                    if(max!=-1)
+                     dp[row][a][b]=max+grid[row][a]+grid[row][b];
+                      if(ans < dp[row][a][b]) 
+                          ans = dp[row][a][b];
+                }
+            }
+        }
+        return ans;
+        
+        
+        
     }
   public int helper(int grid[][],int row,int a,int b,int dp[][][]){
         if(a<0 || a>=grid[0].length || b<0 || b>=grid[0].length)
