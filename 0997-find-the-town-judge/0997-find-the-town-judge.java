2702a250e7e0292d7db5f10a44ee77f9c9142b95
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-02-22 09:40:41 +0530

    Time: 3 ms (71.04%), Space: 53.6 MB (63.27%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -3,16 +3,13 @@ class Solution {
         int arr[]=new int[n+1];
         for(int t[]:trust){
             arr[t[1]]++;
+            arr[t[0]]--;
         }
         int ans=-1;
         for(int i=0;i<=n;i++){
             if(arr[i]==n-1)
                 ans=i;
         }
-     for(int t[]:trust){
-            if(t[0]==ans)
-                return -1;
-        }
-        return ans;
+       return ans;
     }
 }
