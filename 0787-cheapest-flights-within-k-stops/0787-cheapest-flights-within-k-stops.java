a1547b3a4962f932c499a9a53f8c32fb285c372e
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-02-23 14:18:41 +0530

    Time: 6 ms (86.77%), Space: 44 MB (85.04%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,61 +1,40 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-           ArrayList<ArrayList<Pair>> arr=new ArrayList<>();
-  			for(int i=0;i<n;i++) {
-  				arr.add(new ArrayList<Pair>());
-  			}
-  			
-  			for(int fl[]:flights) {
-  				arr.get(fl[0]).add(new Pair(fl[1],fl[2]));
-  			}
-            int dis[]=new int[n];
-  			Arrays.fill(dis, Integer.MAX_VALUE);
-  			Queue<Tuple> queue=new LinkedList<Tuple>();
-			dis[src]=0;
-			queue.add(new Tuple(0,src,0));
-			//System.out.println(node);
-            int stops=0;
-			while(!queue.isEmpty()) {
-				Tuple t=queue.peek();
-                queue.remove();
-                int stop=t.a;
-                int cur=t.b;
-                int d=t.c;
+        List<List<Node>> list=new ArrayList<>();
+        for(int i=0;i<n;i++)
+            list.add(new ArrayList<Node>());
+        for(int f[]:flights){
+            list.get(f[0]).add(new Node(f[1],f[2]));
+        }
+        Queue<int[]> q=new LinkedList<>();
+        int dist[]=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        q.add(new int[]{src,0,0});
+            
+            while(!q.isEmpty()){
+                 int curNode[]=q.poll();
+                 int stop=curNode[2];
                 if(stop>k)
-                    continue;
-                
-				for(Pair child:arr.get(cur)) {
-                    int adjNode = child.a; 
-                    int edW = child.b; 
-					//System.out.println(weight.get(new Weight(curr,child)));
-					if(d+edW<dis[adjNode] && stop<=k) {
-						//System.out.println(child);
-						queue.add(new Tuple(stop+1,adjNode,d+edW));
-						dis[adjNode]=d+edW;
-				}
-				
-			}
-		}
-          
-        
-          return dis[dst]==Integer.MAX_VALUE?-1:dis[dst];
+                     continue;
+                for(Node child:list.get(curNode[0])){
+                    if(child.dis+curNode[1]<dist[child.node]){
+                        dist[child.node]=child.dis+curNode[1];
+                        q.add(new int[]{child.node,child.dis+curNode[1],stop+1});
+                        
+                    }
+                }
+                 
+            }
+        return dist[dst]==Integer.MAX_VALUE?-1:dist[dst];
         
     }
 }
-class Pair{
-    int a,b;
-    Pair(int a,int b){
-       this.a=a;
-        this.b=b; 
+
+class Node{
+    int node;
+    int dis;
+    Node(int node,int dis){
+        this.node=node;
+        this.dis=dis;
     }
-    Pair(){}
-}
-class Tuple{
-    int a,b,c;
-    Tuple(int a,int b,int c){
-        this.a=a;
-        this.b=b;
-        this.c=c;
-    }
-    Tuple(){}
 }
