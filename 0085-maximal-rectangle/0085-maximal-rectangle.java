f7e650f8a36848e8b7a8f8c803842245642bd108
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-04-13 11:00:22 +0530

    Time: 27 ms (22.71%), Space: 44.8 MB (97.89%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,59 +1,54 @@
 class Solution {
-    public int maximalRectangle(char[][] mat) {
-        int max=0;
-        int h[]=new int[mat[0].length];
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]=='0'){
-                    h[j]=0;
+    public int maximalRectangle(char[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int dp[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0){
+                    dp[i][j]=matrix[i][j]=='1'?1:0;
                 }
-                else
-                    h[j]++;
+                else if(matrix[i][j]=='1'){
+                    dp[i][j]=dp[i-1][j]+1;
+                }
             }
-            int area=largestRectangleArea(h);
-            max=Math.max(area,max);
         }
-        return max;
-    }
-    public int largestRectangleArea(int[] heights) {
-        int next[]=nextSmaller(heights);
-        int prev[]=prevSmaller(heights);
-          //System.out.println(Arrays.toString(next));
         int max=0;
-        for(int i=0;i<heights.length;i++){
-            int h=heights[i];
-             if(next[i]==-1)
-                next[i]=heights.length;
-            int b=next[i]-prev[i]-1;
-            max=Math.max(max,b*h);
-            
+        for(int arr[]:dp){
+            int[] prevSmaller=findPrevSmaller(arr);
+            int[] nextSmaller=findNextSmaller(arr);
+            for(int i=0;i<arr.length;i++){
+                int prev=prevSmaller[i];
+                int next=nextSmaller[i];
+                max=Math.max(max,(next-prev-1)*arr[i]);
+            }
         }
         return max;
-        
     }
-    public int[] nextSmaller(int arr[]){
+    public int[] findPrevSmaller(int arr[]){
+        Stack<Integer> st=new Stack<>();
         int ans[]=new int[arr.length];
-        Stack<Integer> st=new Stack<Integer>();
-        for(int i=arr.length-1;i>=0;i--){
-            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
-                st.pop();
-            }
-            ans[i]=st.isEmpty()?-1:st.peek();
-            st.push(i);
-        }
-        return ans;
-    }
-       public int[] prevSmaller(int arr[]){
-        int ans[]=new int[arr.length];
-        Stack<Integer> st=new Stack<Integer>();
         for(int i=0;i<arr.length;i++){
-            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
-                st.pop();
-            }
-            ans[i]=st.isEmpty()?-1:st.peek();
-            st.push(i);
+                while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
+                    st.pop();
+                }
+                ans[i]= st.isEmpty()?-1:st.peek();
+                st.push(i);
         }
-        return ans;
+        // System.out.println("Prev Smaller -> "+Arrays.toString(ans));
+       return ans; 
     }
-
+    public int[] findNextSmaller(int arr[]){
+        Stack<Integer> st=new Stack<>();
+        int ans[]=new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+                while(!st.isEmpty() && arr[i]<=arr[st.peek()]){
+                    st.pop();
+                }
+                ans[i]= st.isEmpty()?arr.length:st.peek();
+                st.push(i);
+        }
+        // System.out.println("Next Smaller -> "+Arrays.toString(ans));
+       return ans; 
+    }
 }
