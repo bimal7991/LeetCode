06958d53fed459bff1c0a7b666efd8ea62559086
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-09-06 23:12:08 +0530

    Time: 613 ms (15.75%), Space: 109.5 MB (93.31%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cs
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cs
@@ -1,32 +1,27 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
  * }
  */
-class Solution {
-    public ListNode modifiedList(int[] nums, ListNode head) {
-        HashSet<Integer> hs=new HashSet<>();
-        for(int i:nums)
-            hs.add(i);
+public class Solution {
+    public ListNode ModifiedList(int[] nums, ListNode head) {
+        HashSet<int> hs=new HashSet<int>(nums);
         ListNode prev=new ListNode();
         ListNode ans=prev;
         while(head!=null){
-            if(hs.contains(head.val)){
-                
-            }
-            else{
+            if(!hs.Contains(head.val)){
                 prev.next=head;
                 prev=prev.next;
             }
             head=head.next;
         }
         prev.next=null;
-        
         return ans.next;
     }
 }
