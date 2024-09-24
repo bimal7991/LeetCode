979a961b8f3dd25dd0589ba536a3f3589834097b
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-09-24 22:04:54 +0530

    Time: 3 ms (46.27%), Space: 59.8 MB (7.77%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,22 +1,17 @@
 class Solution {
-    public boolean isMonotonic(int[] nums) {
-        if(checkIncreasing(nums) || checkDecreasing(nums))
-            return true;
-        else
-            return false;
-    }
-   boolean checkIncreasing(int nums[]){
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i+1]<nums[i])
-                return false;
+   public boolean isMonotonic(int[] A) {
+        int inc=1;
+        int dec=1;
+        for(int i=1; i<A.length; i++){
+            if(A[i] - A[i-1] > 0)
+                inc++;
+            else if(A[i] - A[i-1] < 0)
+                dec++;
+            else {
+                inc++; dec++;
+            }
+                
         }
-       return true;
-    }
-     boolean checkDecreasing(int nums[]){
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i+1]>nums[i])
-                return false;
-        }
-       return true;
+        return inc == A.length || dec == A.length;
     }
 }
