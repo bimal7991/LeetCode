1d97e0b61de5520aae4370aa4e4533b866734423
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-10-05 09:24:02 +0530

    Time: 35 ms (27.51%), Space: 44.8 MB (22.66%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,32 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int freq1[]=new int[26];
-        for(char c:s1.toCharArray()){
-            freq1[c-'a']++;
-        }
-        
-        int i=0;
-        int j=0;
-        int n=s2.length();
-        int freq2[]=new int[26];
-        while(j<n){
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for(char c:s1.toCharArray())
+            hm.put(c,hm.getOrDefault(c,0)+1);
+        int i=0,j=0;
+        HashMap<Character,Integer> map=new HashMap<>();
+        while(j<s2.length()){
             char c=s2.charAt(j);
-            freq2[c-'a']++;
-            
-            if(Arrays.equals(freq1,freq2))
-                return true;
-            else if(freq2[c-'a']>freq1[c-'a']){
-                while(freq2[c-'a']>freq1[c-'a']){
-                    char f=s2.charAt(i);
-                    freq2[f-'a']--;
-                    i++;
-                }
-                if(Arrays.equals(freq1,freq2))
-                return true;
+            map.put(c,map.getOrDefault(c,0)+1);
+            while(map.getOrDefault(c,0)>hm.getOrDefault(c,0)){
+                map.put(s2.charAt(i),map.get(s2.charAt(i))-1);
+                if(map.get(s2.charAt(i))==0)
+                    map.remove(s2.charAt(i));
+                 i++;
             }
+            if(hm.equals(map))
+                return true;
             j++;
-            
         }
         return false;
+    }
+    boolean checkEquals(HashMap<Character,Integer> hm1,HashMap<Character,Integer> hm2){
+        for(char k:hm1.keySet()){
+            if(!hm2.containsKey(k))
+                return false;
+            if(hm1.get(k)>hm2.get(k))
+                return false;
+        }
+        return true;
     }
 }
