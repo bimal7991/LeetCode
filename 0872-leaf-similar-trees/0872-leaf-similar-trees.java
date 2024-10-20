3c07ed1add2e16edbf0d0f510eee61764b1a3ba5
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-10-20 13:23:58 +0530

    Time: 1 ms (18.92%), Space: 41.8 MB (9.49%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -15,31 +15,45 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        
-        ArrayList<Integer> li1=getSequence(root1);
-         ArrayList<Integer> li2=getSequence(root2);
-         return li1.equals(li2);
-               
-    }
-    ArrayList getSequence(TreeNode root){
-        Stack<TreeNode> st=new Stack<>();
-        ArrayList<Integer> li=new ArrayList<>();
-        if(root==null)
-        return li;
-        st.push(root);
-        while(!st.isEmpty()){
-            int s=st.size();
-            for(int i=0;i<s;i++){
-                TreeNode temp=st.pop();
-                if(temp.left==null && temp.right==null){
-                      li.add(temp.val);
+        Stack<TreeNode> s1=new Stack<>();
+        Stack<TreeNode> s2=new Stack<>();
+        s1.add(root1);
+        s2.add(root2);
+        while(!s1.isEmpty() || !s2.isEmpty()){
+            if(s1.isEmpty() || s2.isEmpty())
+                return false;
+            root1=s1.pop();
+            while(root1!=null && !checkLeaf(root1)){
+                if(root1.right!=null)
+                    s1.push(root1.right);
+                if(root1.left!=null){
+                    s1.push(root1.left);
                 }
-                if(temp.left!=null)
-                   st.push(temp.left);
-                 if(temp.right!=null)
-                   st.push(temp.right);    
+                root1=s1.pop();
+            }
+            root2=s2.pop();
+            while(root2!=null && !checkLeaf(root2)){
+                if(root2.right!=null)
+                    s2.push(root2.right);
+                if(root2.left!=null){
+                    s2.push(root2.left);
+                }
+                root2=s2.pop();
+            }
+            
+            if(root1==null && root2!=null)
+                return false;
+            else if(root1!=null && root2==null)
+                return false;
+            else if(root1!=null && root2!=null)
+            {
+                if(root1.val!=root2.val)
+                    return false;
             }
         }
-        return li;
+        return true;
+    }
+    boolean checkLeaf(TreeNode node){
+        return node.left==null && node.right==null;
     }
 }
