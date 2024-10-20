3159ce5d11e208762858a17583aae8c3f4da7de6
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-10-20 23:47:41 +0530

    Time: 7 ms (59.66%), Space: 60.7 MB (19.70%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TreeInfo{
+    boolean isBST;
+    int min,max,sum,totalSum,size;
+    TreeInfo(boolean isBst,int min,int max,int sum){
+        this.isBST=isBst;
+        this.min=min;
+        this.max=max;
+        this.sum=sum;
+    }
+}
+class Solution {
+   
+    public int maxSumBST(TreeNode root) {
+        int ans[]=new int[1];
+        helper(root,ans);
+        return ans[0];
+    }
+    public TreeInfo helper(TreeNode tree,int ans[]){
+        if(tree==null)
+            return new TreeInfo(true,Integer.MAX_VALUE,Integer.MIN_VALUE,0);
+        
+        TreeInfo left=helper(tree.left,ans);
+        TreeInfo right=helper(tree.right,ans);
+        boolean isBST=left.isBST && right.isBST && tree.val>left.max && tree.val<right.min;
+        int sum=left.sum+right.sum+tree.val;
+        if(isBST){
+           ans[0]=Math.max(ans[0],sum);
+                
+        }
+        return new TreeInfo(isBST,Math.min(tree.val,left.min),Math.max(tree.val,right.max),sum);
+
+    }
+    
+}
