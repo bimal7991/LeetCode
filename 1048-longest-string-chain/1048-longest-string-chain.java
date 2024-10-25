636f285dcd9eda9df84b6d768318ae4ebb7d6160
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-10-25 21:00:05 +0530

    Time: 42 ms (67.09%), Space: 44.3 MB (70.76%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,23 +1,43 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Arrays.sort(words,(a,b)->a.length()-b.length());
-        HashMap<String,Integer> hm=new HashMap<>();
-        for(String w:words){
-            hm.put(w,hm.getOrDefault(w,0)+1);
-        }
-        int ans=0;
-        for(String w:words){
-            int max=0;
-            for(int i=0;i<w.length();i++){
-                String key=w.substring(0,i)+w.substring(i+1,w.length());
-                max=Math.max(max,hm.getOrDefault(key,0));
+         Arrays.sort(words,(a,b)->a.length()-b.length());
+        int n=words.length;
+        int max=0;
+        int dp[]=new int[n];
+         for(int i=1;i<n;i++){
+             for(int j=0;j<i;j++){
+                 if(check(words[j],words[i])){
+                     // System.out.println(words[j]+" "+words[i]);
+                     if(dp[i]<dp[j]+1){
+                         dp[i]=dp[j]+1;
+                         max=Math.max(max,dp[i]);
+                     }
+                 }
+             }
+         }
+        return max+1;
+    }
+    public boolean check(String small,String big){
+        if(small.length()+1!=big.length())
+            return false;
+        int i=0,j=0,diff=0;
+        
+        while(i<small.length()){
+            if(small.charAt(i)==big.charAt(j))
+            {
+                i++;
+                j++;
             }
-            hm.put(w,max+1);
-            ans=Math.max(ans,max+1);
+            else{
+                j++;
+                diff++;
+            }
+          if(diff>1)
+              return false;
         }
-        return ans;
-        
-        
-        
+        if(diff<=1)
+            return true;
+        else
+            return false;
     }
 }
