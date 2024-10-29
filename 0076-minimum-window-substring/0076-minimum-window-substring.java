d409b9ef1af68b687c5278f1e4c3060d8b1f93f4
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-10-29 21:07:05 +0530

    Time: 382 ms (5.01%), Space: 45.4 MB (30.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,48 +1,38 @@
 class Solution {
-    public String minWindow(String s1, String s2) {
-     HashMap<Character, Integer> hm=new HashMap<Character, Integer>();
-	for(int i=0;i<s2.length();i++) {
-		char c=s2.charAt(i);
-		if(hm.containsKey(c)) {
-			hm.put(c, hm.get(c)+1);
-		}
-		else {
-			hm.put(c, 1);
-		}
-	}
-	int count=hm.size();
-	
-	
-	int i=0,j=0;
-	String ans="";
-	int min=s1.length()+1;
-	//System.out.println(hm);
-	int flag=0;
-	while(j<s1.length()) {
-		char c=s1.charAt(j);
-		if(hm.containsKey(c)) {
-			hm.put(c, hm.get(c)-1);
-			if(hm.get(c)==0) {
-				count--;
-			}
-		}
-		while(count==0) {
-			if(j-i+1<min) {
-				//System.out.println(i+" "+j);
-				min=j-i+1;
-				ans=s1.substring(i,j+1);
-			}
-			char c1=s1.charAt(i);
-			if(hm.containsKey(c1)) {
-				hm.put(c1, hm.get(c1)+1);
-				if(hm.get(c1)>0) {
-					count++;
-				}
-			}
-			i++;
-		}
-		j++;
-	}  
+    public String minWindow(String s, String t) {
+        HashMap<Character,Integer> hm=new HashMap<>();
+        HashMap<Character,Integer> sub=new HashMap<>();
+        for(char c:t.toCharArray())
+            hm.put(c,hm.getOrDefault(c,0)+1);
+        int j=0,i=0;
+        int max=s.length()+1;
+        int count=0;
+        String ans="";
+        while(j<s.length()){
+             char c=s.charAt(j);
+            if(hm.containsKey(c)){
+                sub.put(c,sub.getOrDefault(c,0)+1);
+            }
+            while(checkequals(hm,sub)){
+                if(j-i+1<max){
+                    max=Math.min(max,j-i+1);
+                    ans=s.substring(i,j+1);
+                }
+                if(sub.containsKey(s.charAt(i))){
+                     sub.put(s.charAt(i),sub.get(s.charAt(i))-1);
+                }
+                i++;
+            }
+            j++;
+            
+        }
         return ans;
+    }
+    public boolean checkequals(HashMap<Character,Integer> hm1,HashMap<Character,Integer> hm2){
+        for(char key:hm1.keySet()){
+            if(hm2.getOrDefault(key,0)<hm1.get(key))
+                return false;
+        }
+        return true;
     }
 }
