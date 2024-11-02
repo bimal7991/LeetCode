4daad8e730c2d00d67de8beffbeb57a5bdf4bbc3
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-02 21:00:13 +0530

    Time: 81 ms (53.03%), Space: 102.9 MB (9.21%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,7 +1,7 @@
 class Solution {
+    int max=-1;
     public int longestCycle(int[] edges) {
-        int n=edges.length;
-                int inDegree[]=new int[n];
+               int n=edges.length;
                 ArrayList<Integer> arr[]=new ArrayList[n];
 				for(int i=0;i<n;i++) {
 					arr[i]=new ArrayList<Integer>();
@@ -10,35 +10,57 @@ class Solution {
 					int u=i,v=edges[i];
                          if(v!=-1){
 					arr[u].add(v);
-               
-                    inDegree[v]++;	
-                         }
+                   }
 				}
-
+                boolean vis[]=new boolean[n];
+                boolean dfs[]=new boolean[n];
+                int dis[]=new int[n];
+                for(int i=0;i<n;i++){
+                    if(!vis[i])
+                        dfs(i,arr,vis,dfs,dis,0);
+                }
 				
-				ArrayList<Integer> ans=new ArrayList<Integer>();
-				bfs(inDegree,ans,arr);
-				int max=-1;
-			    for(int i=0;i<n;i++){
-                    if(inDegree[i]>0){
-                        max=Math.max(max,getLength(inDegree,edges,i));
-                    }
-                }		
+				// bfs(inDegree,arr);
+				// int max=-1;
+				// for(int i=0;i<n;i++){
+				// if(inDegree[i]>0){
+				// max=Math.max(max,getLength(inDegree,edges,i));
+				// }
+				// }		
               return max;   
 				
 			}
-      int getLength(int inDegree[],int edges[],int node){
-          int l=0;
-          while(inDegree[node]!=0){
-              inDegree[node]--;
-              node=edges[node];
-              l++;
-          }
-          return l;
-      }
+      // int getLength(int inDegree[],int edges[],int node){
+      //     int l=0;
+      //     while(inDegree[node]!=0){
+      //         inDegree[node]--;
+      //         node=edges[node];
+      //         l++;
+      //     }
+      //     return l;
+      // }
+    public void dfs(int node,ArrayList<Integer> arr[],boolean vis[],boolean dfs[],int dis[],int d){
+        if(vis[node])
+            return;
+        vis[node]=true;
+        dfs[node]=true;
+        dis[node]=d;
+        for(int child:arr[node]){
+            if(!vis[child]){
+                dfs(child,arr,vis,dfs,dis,d+1);
+            }
+            else{
+                if(dfs[child]){
+                    // System.out.println(d-dis[child]);
+                    max=Math.max(max,d-dis[child]+1);
+                }
+            }
+        }
+        dfs[node]=false;
+    }
         
     
-   public void bfs( int[] inDegree, ArrayList<Integer> ans, ArrayList<Integer>[] arr) {
+   public void bfs( int[] inDegree, ArrayList<Integer>[] arr) {
 		// TODO Auto-generated method stub
 		Queue<Integer> q=new LinkedList<Integer>();
 		for(int i=0;i<inDegree.length;i++) {
@@ -48,7 +70,6 @@ class Solution {
 		}		
 		while(!q.isEmpty()) {
 			int parent=q.poll();
-			ans.add(parent);
 			for(int child:arr[parent]) {
 				inDegree[child]--;
 				if(inDegree[child]==0) {
