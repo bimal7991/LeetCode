82e835c4567364092232baf30942339850af71e2
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-02 21:58:53 +0530

    Time: 9 ms (95.03%), Space: 48.8 MB (34.84%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,73 +1,37 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-          ArrayList<NodeDis> arr[]=new ArrayList[n+1];
-	  			for(int i=0;i<=n;i++) {
-	  				arr[i]=new ArrayList<NodeDis>();
-	  			}
-	  			
-	  			for(int i=0;i<times.length;i++) {
-	  				int u=times[i][0],v=times[i][1],w=times[i][2];
-	  				arr[u].add(new NodeDis(v,w));
-	  				
-	  				
-	  			}
-	  			int dis[]=new int[n+1];
-	  			Arrays.fill(dis, 999999);
-	  			sortestDistance(k, arr,dis);
-	  			int max=-9999999;
-	  			for(int i=1;i<=n;i++) {
-                    System.out.print(dis[i]+" ");
-                    if(dis[i]==999999)
-                        return -1;
-                    else 
-	  				max=Math.max(dis[i],max);
-	  			}
-        return max;
-        
+        List<NodeDis> list[]=new ArrayList[n];
+        for(int i=0;i<n;i++)
+            list[i]=new ArrayList<>();
+        for(int t[]:times){
+            list[t[0]-1].add(new NodeDis(t[1]-1,t[2]));
+        }
+        int res[]=new int[n];
+        Arrays.fill(res,Integer.MAX_VALUE);
+        res[k-1]=0;
+        PriorityQueue<NodeDis> pq=new PriorityQueue<>((a,b)->a.weight-b.weight);
+        pq.add(new NodeDis(k-1,0));
+        while(!pq.isEmpty()){
+            NodeDis curr=pq.poll();
+            for(NodeDis next:list[curr.node]){
+                if(res[next.node]>res[curr.node]+next.weight){
+                    res[next.node]=res[curr.node]+next.weight;
+                    pq.add(next);
+                }
+            }
+        }
+        // System.out.println(Arrays.toString(res));
+        int max=0;
+        for(int i:res)
+            max=Math.max(max,i);
+        return max==Integer.MAX_VALUE?-1:max;
     }
-        
-  public  void sortestDistance(int i, ArrayList<NodeDis>[] arr, int[] dis) {
-		PriorityQueue<NodeDis> pq=new PriorityQueue<NodeDis>((a,b)->a.weight-b.weight); 
-		
-		pq.add(new NodeDis(i,0));
-		dis[i]=0;
-		while(!pq.isEmpty()) {
-			NodeDis parent=pq.poll();
-			for(NodeDis child: arr[parent.getNode()]) {
-				if(dis[parent.getNode()]+child.getWeight()<dis[child.getNode()]) {
-					dis[child.getNode()]=dis[parent.getNode()]+child.getWeight();
-					pq.add(new NodeDis(child.getNode(),dis[child.getNode()]));
-				}
-			}
-			
-		}
-		
-	}
 }
-class NodeDis {
-	int node;
-	int weight;
-	public NodeDis() {
-		
-	}
-	
-	public NodeDis(int node, int weight) {
-		this.node = node;
-		this.weight = weight;
-	}
-	public int getNode() {
-		return node;
-	}
-	public void setNode(int node) {
-		this.node = node;
-	}
-	public int getWeight() {
-		return weight;
-	}
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
-	
-	
-	
+class NodeDis{
+    int node;
+    int weight;
+    NodeDis(int n,int w){
+        node=n;
+        weight=w;
+    }
 }
