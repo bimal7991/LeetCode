890fa4edfd2b99597f745598ac635a1b3b4276fe
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-06 23:46:42 +0530

    Time: 39 ms (17.90%), Space: 45.4 MB (28.10%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,20 +1,25 @@
 class Solution {
     public int jump(int[] nums) {
-        int dp[]=new int[nums.length];
-        return helper(nums,0,dp);
-    }
-    int helper(int nums[],int ind,int dp[]){
-        if(ind>=nums.length-1 )
-            return 0;
-        if(nums[ind]==0)
-            return nums.length;
-        if(dp[ind]!=0)
-            return dp[ind];
-        int temp=nums.length;
-        for(int i=1;i<=nums[ind] ;i++){
-            temp=Math.min(temp,1+helper(nums,i+ind,dp));
+        int dp[]=new int[nums.length+1];
+        Arrays.fill(dp,Integer.MAX_VALUE-1);
+        dp[0]=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<=i+nums[i] && j<nums.length;j++){
+              dp[j]=Math.min(dp[j],dp[i]+1); 
+            }
         }
+        // System.out.println(Arrays.toString(dp));
+       return dp[nums.length-1];
+        // return helper(0,nums);
         
-        return dp[ind]=temp;
+    }
+    int helper(int index,int nums[]){
+        if(index>=nums.length-1)
+            return 0;
+        int min=Integer.MAX_VALUE-1;
+        for(int i=1;i<=nums[index];i++){
+            min=Math.min(1+helper(index+i,nums),min);
+        }
+        return min;
     }
 }
