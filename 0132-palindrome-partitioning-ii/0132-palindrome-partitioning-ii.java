8c54315df48656350bd08c7976660a7c32e0c4ad
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-07 21:53:42 +0530

    Time: 2413 ms (32.26%), Space: 73.6 MB (12.86%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,30 +1,26 @@
 class Solution {
     public int minCut(String s) {
-        int dp[][]=new int[s.length()][s.length()];
-        for(int d[]:dp)
-        {
-            Arrays.fill(d,-1);
-        }
+    int dp[][]=new int[s.length()][s.length()];
+    for(int d[]:dp)
+        Arrays.fill(d,-1);
         return helper(0,0,s,dp);
     }
     public int helper(int i,int j,String s,int dp[][]){
-        if(j==s.length())
-        {
-            if(isPalindrome(i,j-1,s))
-                return 0;
-            else
-                return 20005;
-        }
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
+         
+          if(j==s.length()-1){
+               if(isPalindrome(i,j,s))
+                 return 0;
+              return 200007;
+          }
+       if(dp[i][j]!=-1)
+           return dp[i][j];
         if(isPalindrome(i,j,s)){
-           int a=1+helper(j+1,j+1,s,dp);
-           int b=helper(i,j+1,s,dp);
-            return dp[i][j]= Math.min(a,b);
+            int inc=1+helper(j+1,j+1,s,dp);
+            int exc=helper(i,j+1,s,dp);
+            return dp[i][j]= Math.min(inc,exc);
         }
         else{
-           return dp[i][j]=helper(i,j+1,s,dp);
+            return dp[i][j]=helper(i,j+1,s,dp);
         }
     }
     boolean isPalindrome(int i,int j,String s){
