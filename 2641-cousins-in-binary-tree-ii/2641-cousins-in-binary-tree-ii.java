4d8362e54a68e28df5166e5bffb42ccd36f9c800
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-09 17:05:48 +0530

    Time: 25 ms (56.33%), Space: 76.3 MB (62.01%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -15,26 +15,58 @@
  */
 class Solution {
    public TreeNode replaceValueInTree(TreeNode root) {
-    Map<Integer, Integer> m = new HashMap<>();
-    dfs(root, 0, m);
-    return dfs1(root, 0, new TreeNode(0), m);
-}
-private void dfs(TreeNode r, int l, Map<Integer, Integer> m) {
-    if (r == null) return;
-    m.put(l, m.getOrDefault(l, 0) + r.val);
-    dfs(r.left, l + 1, m);
-    dfs(r.right, l + 1, m);
-}
-private TreeNode dfs1(TreeNode r, int l, TreeNode curr, Map<Integer, Integer> m) {
-    int sum_of_cousins = m.getOrDefault(l + 1, 0) - ((r.left != null) ? r.left.val : 0) - ((r.right != null) ? r.right.val : 0);
-    if (r.left != null) {
-        curr.left = new TreeNode(sum_of_cousins);
-        dfs1(r.left, l + 1, curr.left, m);
-    }
-    if (r.right != null) {
-        curr.right = new TreeNode(sum_of_cousins);
-        dfs1(r.right, l + 1, curr.right, m);
-    }
-    return curr;
-}
+      Queue<TreeNode> queue=new LinkedList<>();
+       HashMap<Integer,Integer> hm=new HashMap<>();
+      queue.add(root);
+      root.val=0;
+       hm.put(0,root.val);
+       int level=1;
+       while(!queue.isEmpty()){
+           int s=queue.size();
+          int levelSum=0;
+           for(int i=0;i<s;i++){
+               TreeNode temp=queue.poll();
+                int sum=0;
+                if(temp.left!=null){
+                    sum=sum+temp.left.val;
+                    queue.add(temp.left);
+                }
+               if(temp.right!=null){
+                   sum+=temp.right.val;
+                   queue.add(temp.right);
+               }
+                if(temp.left!=null){
+                    temp.left.val=sum;
+                }
+                if(temp.right!=null){
+                   temp.right.val=sum;
+               }
+               levelSum+=sum;
+           }
+           hm.put(level,levelSum);
+           level++;
+       }
+       // return root;
+      queue.add(root);
+       root.val=0;
+       level=0;
+       while(!queue.isEmpty()){
+           int s=queue.size();
+           int lsum=hm.get(level);
+           for(int i=0;i<s;i++){
+               TreeNode temp=queue.poll();
+               temp.val=lsum-temp.val;
+                int sum=0;
+                if(temp.left!=null){
+                    queue.add(temp.left);
+                }
+               if(temp.right!=null){
+                   queue.add(temp.right);
+               }
+
+           }
+           level++;
+       }
+       return root;
+   }
 }
