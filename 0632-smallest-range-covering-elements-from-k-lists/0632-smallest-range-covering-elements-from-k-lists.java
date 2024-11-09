7dc2011acd49ee6b2843d2214ec83383e4d250ff
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-10 00:01:47 +0530

    Time: 87 ms (28.35%), Space: 66.1 MB (5.03%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,45 +1,41 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
+        PriorityQueue<Element> pq=new PriorityQueue<>((a,b)->a.val-b.val);
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int range=Integer.MAX_VALUE;
+        int start=-1;
+        int end=-1;
+        for(int i=0;i<nums.size();i++){
+            pq.add(new Element(nums.get(i).get(0),i,0));
+            max=Math.max(nums.get(i).get(0),max);
+        }
+        while(pq.size()==nums.size()){
+            Element curr=pq.poll();
+            if(max-curr.val<range){
+                start=curr.val;
+                end=max;
+                range=max-curr.val;
+            }
+            if(curr.index+1<nums.get(curr.row).size()){
+                 curr.index=curr.index+1;
+                 curr.val=nums.get(curr.row).get(curr.index);
+                 pq.offer(curr);
+                 max=Math.max(max,curr.val);
+            }
+        }
+        return new int[]{start,end};
+    }
         
-   PriorityQueue<Element> pq = new PriorityQueue<Element>((a,b)->a.val-b.val);
-		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-		for (int i = 0; i < nums.size(); i++) {
-			Element e = new Element(i, 0, nums.get(i).get(0));
-			pq.offer(e);
-			max = Math.max(max, nums.get(i).get(0));
-		}
-		int range = Integer.MAX_VALUE;
-		int start = -1, end = -1;
-		while (pq.size() == nums.size()) {
-
-			Element curr = pq.poll();
-			if (max - curr.val < range) {
-				range = max - curr.val;
-				start = curr.val;
-				end = max;
-			}
-			if (curr.idx + 1 < nums.get(curr.row).size()) {
-				curr.idx = curr.idx + 1;
-				curr.val = nums.get(curr.row).get(curr.idx);
-				pq.offer(curr);
-				if (curr.val > max) {
-					max = curr.val;
-				}
-			}
-		}
-
-		return new int[] { start, end };
-	}
+  
 }
-
-	class Element {
-		int val;
-		int idx;
-		int row;
-
-		public Element(int r, int i, int v) {
-			val = v;
-			idx = i;
-			row = r;
-		}
-	}
+class Element {
+   int val;
+   int row;
+   int index;
+   Element(int v,int r,int i){
+       val=v;
+       row=r;
+       index=i;
+   }
+}
