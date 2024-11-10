b8f9d303791815d0dfb422cda05e3c8049ca2006
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-10 12:50:27 +0530

    Time: 747 ms (6.05%), Space: 48.2 MB (7.37%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.java
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -1,27 +1,27 @@
 class MyCalendarTwo {
 
-    private TreeMap<Integer, Integer> map;    
+    private TreeMap<Integer, Integer> treemap;    
     
     public MyCalendarTwo() {
-        map = new TreeMap<>();
+        treemap = new TreeMap<>();
     }
     
     public boolean book(int start, int end) {
-        map.put(start, map.getOrDefault(start, 0) + 1);
-        map.put(end, map.getOrDefault(end, 0) - 1);
-        int count = 0;
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            count += entry.getValue();
-            if(count > 2) {
-                map.put(start, map.get(start) - 1);
-                if(map.get(start) == 0) {
-                    map.remove(start);
-                }
-                map.put(end, map.get(end) + 1);
-                if(map.get(end) == 0) {
-                    map.remove(end);
-                }
+        treemap.put(start,treemap.getOrDefault(start,0)+1);
+        treemap.put(end,treemap.getOrDefault(end,0)-1);
+        int count=0;
+        for(int key:treemap.keySet()){
+            count+=treemap.get(key);
+            if(count>2)
+            {
+                treemap.put(start,treemap.getOrDefault(start,0)-1);
+                treemap.put(end,treemap.getOrDefault(end,0)+1);
+                if(treemap.get(start)==0)
+                    treemap.remove(start);
+                if(treemap.get(end)==0)
+                    treemap.remove(end);
                 return false;
+                
             }
         }
         return true;
