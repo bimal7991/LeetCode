41392d33e3aee5154127a4d298798b208b31f2f5
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-10 10:08:52 +0530

    Time: 22 ms (38.06%), Space: 43.1 MB (31.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -4,29 +4,37 @@ class Solution {
         for(char c:s1.toCharArray())
             hm.put(c,hm.getOrDefault(c,0)+1);
         int i=0,j=0;
-        HashMap<Character,Integer> map=new HashMap<>();
+        int count=hm.size();
         while(j<s2.length()){
             char c=s2.charAt(j);
-            map.put(c,map.getOrDefault(c,0)+1);
-            while(map.getOrDefault(c,0)>hm.getOrDefault(c,0)){
-                map.put(s2.charAt(i),map.get(s2.charAt(i))-1);
-                if(map.get(s2.charAt(i))==0)
-                    map.remove(s2.charAt(i));
-                 i++;
+            if(hm.containsKey(c)){
+              hm.put(c,hm.getOrDefault(c,0)-1);
+              if(hm.get(c)==0)
+                count--;
+              
             }
-            if(hm.equals(map))
-                return true;
+             while(count==0){
+                  if(j-i+1==s1.length())
+                      return true;
+                    if(hm.containsKey(s2.charAt(i))){
+                           hm.put(s2.charAt(i),hm.getOrDefault(s2.charAt(i),0)+1);
+                           if(hm.get(s2.charAt(i))>0)
+                               count++;
+                    }
+                    i++;
+           }
+              
+             
+            // while(map.getOrDefault(c,0)>hm.getOrDefault(c,0)){
+            //     map.put(s2.charAt(i),map.get(s2.charAt(i))-1);
+            //     if(map.get(s2.charAt(i))==0)
+            //         map.remove(s2.charAt(i));
+            //      i++;
+            // }
+            // if(hm.equals(map))
+            //     return true;
             j++;
         }
         return false;
-    }
-    boolean checkEquals(HashMap<Character,Integer> hm1,HashMap<Character,Integer> hm2){
-        for(char k:hm1.keySet()){
-            if(!hm2.containsKey(k))
-                return false;
-            if(hm1.get(k)>hm2.get(k))
-                return false;
-        }
-        return true;
     }
 }
