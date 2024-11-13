bc76a00319f070d192808b7267871a313532ded0
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-13 13:26:46 +0530

    Time: 64 ms (49.28%), Space: 57.3 MB (50.72%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,47 +1,33 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-    int n = nums.length;
-    Arrays.sort(nums);
-    long count = 0;
-    for (int i = 0; i < n; i++) {
-        int target1 = lower-nums[i];
-        int target2= upper-nums[i];
-        int j = binarySearch(nums, i+1,n-1,target1);
-        int k = binarySearch2(nums, i+1,n-1,target2);
-        if(k!=-1 && j!=-1)
-        count += k+1-j;
-    }
-    return count;
-}
-
- int binarySearch(int[] nums, int left, int right,int target) {
-    int ans=-1;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (nums[mid]>=target) {
-            ans=mid;
-            right=mid-1;
-            
-        } else {
-            
-            left = mid+1;
+        Arrays.sort(nums);
+        long count1=0;
+        long count2=0;
+        for(int i=0;i<nums.length;i++){
+            int t1=lower-1-nums[i];
+            int t2=upper-nums[i];
+            int ind1=findIndex(nums,i,t1);
+            int ind2=findIndex(nums,i,t2);
+            // System.out.println(ind1+" "+ind2);
+            count1=count1+ (ind1==-1?0:ind1-i);
+            count2=count2+ (ind2==-1?0:ind2-i);
         }
+        return count2-count1;
+        
     }
-    return ans;
-}
-int binarySearch2(int[] nums, int left, int right,int target) {
-    int ans=-1;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (nums[mid]<=target) {
-            ans=mid;
-            left=mid+1;
-            
-        } else {
-            
-            right = mid - 1;
+    int findIndex(int nums[],int low,int target){
+        int high=nums.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]<=target){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
+        return ans;
     }
-    return ans;
-  }
 }
