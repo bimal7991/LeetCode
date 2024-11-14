2ddddd680e0088721db6220eca9de9c9bcedb46f
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-14 12:21:37 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (14.12%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.java
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.java
@@ -14,25 +14,19 @@
  * }
  */
 class Solution {
-   private int steps = 0; 
-
-public int distributeCoins(TreeNode root) {
-    postorder(root);
-    return steps;
-}
-
-// return coins of this level
-private int postorder(TreeNode node) {
-    if(node == null) return 0;
-    
-    // coin from children -- post-order traversal
-    int coin = postorder(node.left) + postorder(node.right);
-	
-    // current node coin
-    if(node.val == 0) coin += -1; // current node need one coin
-    else coin += node.val - 1; // keep one coin for current node
-    
-    steps += Math.abs(coin); // each coin move up to parent node need 1 step
-    return coin; 
-}
+    int ans=0;
+    public int distributeCoins(TreeNode root) {
+        helper(root);
+        return ans;
+    }
+    public int[] helper(TreeNode root){
+        if(root==null)
+            return new int[]{0,0};
+        int left[]=helper(root.left);
+        int right[]=helper(root.right);
+        int size=left[0]+right[0]+1;  //1 
+        int coin=left[1]+right[1]+root.val;
+        ans=ans+Math.abs(size-coin);
+        return new int[]{size,coin};
+    }
 }
