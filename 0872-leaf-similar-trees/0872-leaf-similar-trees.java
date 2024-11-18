62122cdc938bb0fa18909d2de95a0e9283ca3af1
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-18 20:03:13 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (15.71%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -14,46 +14,21 @@
  * }
  */
 class Solution {
+   
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        Stack<TreeNode> s1=new Stack<>();
-        Stack<TreeNode> s2=new Stack<>();
-        s1.add(root1);
-        s2.add(root2);
-        while(!s1.isEmpty() || !s2.isEmpty()){
-            if(s1.isEmpty() || s2.isEmpty())
-                return false;
-            root1=s1.pop();
-            while(root1!=null && !checkLeaf(root1)){
-                if(root1.right!=null)
-                    s1.push(root1.right);
-                if(root1.left!=null){
-                    s1.push(root1.left);
-                }
-                root1=s1.pop();
-            }
-            root2=s2.pop();
-            while(root2!=null && !checkLeaf(root2)){
-                if(root2.right!=null)
-                    s2.push(root2.right);
-                if(root2.left!=null){
-                    s2.push(root2.left);
-                }
-                root2=s2.pop();
-            }
-            
-            if(root1==null && root2!=null)
-                return false;
-            else if(root1!=null && root2==null)
-                return false;
-            else if(root1!=null && root2!=null)
-            {
-                if(root1.val!=root2.val)
-                    return false;
-            }
-        }
-        return true;
+        List<Integer> list1=new ArrayList<>();
+        List<Integer> list2=new ArrayList<>();
+        traverse(root1,list1);
+         traverse(root2,list2);
+        return list1.equals(list2);
     }
-    boolean checkLeaf(TreeNode node){
-        return node.left==null && node.right==null;
+    public void traverse(TreeNode root,List<Integer> list){
+        if(root==null)
+            return ;
+        if(root.left==null && root.right==null){
+            list.add(root.val);
+        }
+        traverse(root.left,list);
+        traverse(root.right,list);
     }
 }
