a6f9b0227d55aee9303fbda425a9c08a5ede305d
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-18 09:38:22 +0530

    Time: 4 ms (28.91%), Space: 44.7 MB (99.66%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,24 +1,24 @@
 class Solution {
-    public int candy(int[] rat) {
-        int n=rat.length;
-        int arr[]=new int[n];
-        int count=0;
-        Arrays.fill(arr,1);
-        for(int i=1;i<n;i++){
-            if(rat[i]>rat[i-1]){
-                arr[i]=arr[i-1]+1;
-            }
-            
-        }
-        for(int i=n-2;i>=0;i--){
-            if(rat[i]>rat[i+1]){
-                arr[i]=Math.max(arr[i],arr[i+1]+1);
-            }
-        }
+    public int candy(int[] nums) {
+       int left[]=new int[nums.length];
+       int right[]=new int[nums.length];
+       Arrays.fill(left,1);
+       Arrays.fill(right,1);
+       for(int i=1;i<nums.length;i++){
+           if(nums[i]>nums[i-1])
+               left[i]=left[i-1]+1;
+       }
+        for(int i=nums.length-2;i>=0;i--){
+           if(nums[i]>nums[i+1])
+               right[i]=right[i+1]+1;
+       }
+        // System.out.println(Arrays.toString(left));
+        //  System.out.println(Arrays.toString(right));
         int sum=0;
-        for(int I:arr)
-            sum=sum+I;
+        for(int i=0;i<nums.length;i++){
+            sum+=Math.max(left[i],right[i]);
+        }
         return sum;
-    }
         
+    }
 }
