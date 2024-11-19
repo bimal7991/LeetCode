9213d23065d993c82fee89779ed89fab4b9b4d91
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-19 21:04:40 +0530

    Time: 55 ms (34.86%), Space: 58.4 MB (45.62%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,28 +1,24 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int i=0,j=0;
-        long sum=0;
-        long max=0;
-        while(j<nums.length){
-            sum=sum+nums[j];
-            hm.put(nums[j],hm.getOrDefault(nums[j],0)+1);
-            if(j-i+1<k)
-                j++;
-            else if(j-i+1==k){
-                
-                if(hm.size()==k){
+       HashMap<Integer,Integer> hm=new HashMap<>();
+           long sum=0;
+           long max=0;
+           int i=0,j=0;
+           while(j<nums.length){
+               sum=sum+nums[j];
+               hm.put(nums[j],hm.getOrDefault(nums[j],0)+1);
+               if(j-i+1==k){
+                 if(hm.size()==k){
                     max=Math.max(max,sum);
-                }
-                    hm.put(nums[i],hm.get(nums[i])-1);
-                    sum=sum-nums[i];
-                    if(hm.get(nums[i])==0)
-                        hm.remove(nums[i]);
-                    i++;
-                j++;
-            }
-            
-        }
-        return max;
+                 }
+                 sum=sum-nums[i];
+                 hm.put(nums[i],hm.getOrDefault(nums[i],0)-1);
+                 if(hm.get(nums[i])==0)
+                     hm.remove(nums[i]);
+                 i++;    
+               }
+               j++;
+           }
+           return max;
     }
 }
