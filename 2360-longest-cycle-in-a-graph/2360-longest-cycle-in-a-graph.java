6068500de9afe75d192809bbe9649d197e62207c
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-19 23:58:08 +0530

    Time: 33 ms (61.69%), Space: 67.8 MB (58.57%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,81 +1,34 @@
 class Solution {
-    int max=-1;
+   int max=-1;
     public int longestCycle(int[] edges) {
-               int n=edges.length;
-                ArrayList<Integer> arr[]=new ArrayList[n];
-				for(int i=0;i<n;i++) {
-					arr[i]=new ArrayList<Integer>();
-				}
-				for(int i=0;i<edges.length;i++) {
-					int u=i,v=edges[i];
-                         if(v!=-1){
-					arr[u].add(v);
-                   }
-				}
-                boolean vis[]=new boolean[n];
-                boolean dfs[]=new boolean[n];
-                int dis[]=new int[n];
-                for(int i=0;i<n;i++){
-                    if(!vis[i])
-                        dfs(i,arr,vis,dfs,dis,0);
-                }
-				
-				// bfs(inDegree,arr);
-				// int max=-1;
-				// for(int i=0;i<n;i++){
-				// if(inDegree[i]>0){
-				// max=Math.max(max,getLength(inDegree,edges,i));
-				// }
-				// }		
-              return max;   
-				
-			}
-      // int getLength(int inDegree[],int edges[],int node){
-      //     int l=0;
-      //     while(inDegree[node]!=0){
-      //         inDegree[node]--;
-      //         node=edges[node];
-      //         l++;
-      //     }
-      //     return l;
-      // }
-    public void dfs(int node,ArrayList<Integer> arr[],boolean vis[],boolean dfs[],int dis[],int d){
+        int n=edges.length;
+        boolean vis[]=new boolean[n];
+        boolean dfsVis[]=new boolean[n];
+        int dis[]=new int[n];
+        for(int i=0;i<n;i++){
+            dfs(vis,dfsVis,edges,i,0,dis);
+        }
+         return max;
+    }
+    void dfs(boolean vis[],boolean dfsVis[],int edges[],int node,int d,int dis[]){
         if(vis[node])
-            return;
+           return;
         vis[node]=true;
-        dfs[node]=true;
+        dfsVis[node]=true;
         dis[node]=d;
-        for(int child:arr[node]){
-            if(!vis[child]){
-                dfs(child,arr,vis,dfs,dis,d+1);
-            }
-            else{
-                if(dfs[child]){
-                    // System.out.println(d-dis[child]);
-                    max=Math.max(max,d-dis[child]+1);
-                }
+        int c=edges[node];
+        if(c==-1){
+            dfsVis[node]=false;
+            return;
+        }
+        if(!vis[c]){
+            dfs(vis,dfsVis,edges,c,d+1,dis);
+        }
+        else{
+            if(dfsVis[c]){
+                max=Math.max(max,d-dis[c]+1);
             }
         }
-        dfs[node]=false;
+        dfsVis[node]=false;   
     }
-        
-    
-   public void bfs( int[] inDegree, ArrayList<Integer>[] arr) {
-		// TODO Auto-generated method stub
-		Queue<Integer> q=new LinkedList<Integer>();
-		for(int i=0;i<inDegree.length;i++) {
-			if(inDegree[i]==0) {
-				q.add(i);
-			}
-		}		
-		while(!q.isEmpty()) {
-			int parent=q.poll();
-			for(int child:arr[parent]) {
-				inDegree[child]--;
-				if(inDegree[child]==0) {
-					q.add(child);
-				}
-			}
-		}
-	}
 }
