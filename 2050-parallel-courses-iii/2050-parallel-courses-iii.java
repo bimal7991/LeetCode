f312189d817d0b34528b469e274da1c79510c54e
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-11-19 23:13:53 +0530

    Time: 19 ms (80.14%), Space: 69.5 MB (76.73%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,39 +1,44 @@
 class Solution {
     public int minimumTime(int n, int[][] relations, int[] time) {
-        List<Integer> list[]=new ArrayList[n];
-        for(int i=0;i<n;i++){
-            list[i]=new ArrayList<>();
+       int indegree[]=new int[n+1];
+        int finishTime[]=new int[n+1];
+        ArrayList<Integer> adjList[]=new ArrayList[n+1];
+        for(int i=0;i<=n;i++)
+           adjList[i]=new ArrayList<>();
+       for(int rel[]:relations){
+           indegree[rel[1]]++;
+           adjList[rel[0]].add(rel[1]);
+       }
+       Queue<Integer> q=new LinkedList<>();
+       for(int i=1;i<=n;i++){
+        if(indegree[i]==0){
+             q.add(i);
+             finishTime[i]=time[i-1];
         }
-        int indeg[]=new int[n];
-        for(int rel[]:relations){
-            list[rel[0]-1].add(rel[1]-1);
-            indeg[rel[1]-1]++;
-        }
-        Queue<Integer> q=new LinkedList<>();
-        int res[]=new int[n];
-        for(int i=0;i<n;i++)
-            if(indeg[i]==0){
-                q.add(i);
-                res[i]=time[i];
+       }
+        // System.out.println(Arrays.toString(finishTime));
+       while(!q.isEmpty()){
+           int s=q.size();
+           for(int i=0;i<s;i++){
+                 int p=q.poll();
+                 for(int t:adjList[p]){
+                     if(finishTime[t]<time[t-1]+finishTime[p]){
+                        finishTime[t]=time[t-1]+finishTime[p];
+                    }
+                 indegree[t]--;  
+                 if(indegree[t]==0){
+                    q.add(t);
+                    
+                 }
             }
-        
-        while(!q.isEmpty()){
-            int s=q.size();
-            for(int i=0;i<s;i++){
-                int u=q.poll();
-                for(int v:list[u])
-                {
-                    if(res[v]<res[u]+time[v])
-                        res[v]=res[u]+time[v];
-                    indeg[v]--;
-                    if(indeg[v]==0)
-                        q.add(v);
-                }
-            }
         }
-        int max=0;
-        for(int i=0;i<n;i++)
-            max=Math.max(max,res[i]);
-        return max;
+
+       } 
+       int max=0;
+       for(int i=1;i<=n;i++){
+           max=Math.max(finishTime[i],max);
+       } 
+       return max;  
+
     }
 }
