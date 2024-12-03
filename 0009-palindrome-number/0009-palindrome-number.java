fde29e2f6ded19a7819d4accb6a39db1c543aafd
Bimal Paul <55895863+bimal7991@users.noreply.github.com>
2024-12-03 19:28:59 +0530

    Time: 4 ms (100.00%), Space: 44 MB (41.12%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        // Reverse half of the number
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Check if the original number matches the reversed half
+        return x == reversedHalf || x == reversedHalf / 10;
+    
+
+           
+        
+    }
+}
